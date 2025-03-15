4c80e71bf80611cd783c19eccaa3b32b5d3f47ea
gendago <gendagogendago@gmail.com>
2025-03-15 22:52:48 +0800

    feat: 添加至臻站源

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -4,8 +4,9 @@ import mogg from "./spider/video/mogg.js";
 import leijing from "./spider/video/leijing.js";
 import panta from "./spider/video/panta.js";
 import wogg from "./spider/video/wogg.js";
+import zhizhen from "./spider/video/zhizhen.js";
 
-const spiders = [duoduo, mogg, leijing, panta, wogg, baseset];
+const spiders = [duoduo, mogg, leijing, panta, wogg, zhizhen, baseset];
 const spiderPrefix = '/spider';
 
 /**
